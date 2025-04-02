46aca317b37ebd8ef1eaa706d182b26ed45e1119
Ligg <wenzhouli06@gmail.com>
2025-04-02 16:12:15 +0800

    项目初始化

--- src/main/java/com/ligg/controller/oauth/SearchAnimeController.java
+++ src/main/java/com/ligg/controller/oauth/SearchAnimeController.java
@@ -1,21 +1,12 @@
 package com.ligg.controller.oauth;
 
-import com.ligg.service.anime.SearchAnimeService;
-import com.ligg.utils.Result;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/anime/search")
 public class SearchAnimeController {
-
-    @Autowired
-    private SearchAnimeService animeService;
 
 
 }
